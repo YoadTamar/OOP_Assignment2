d80e8fa24043378c738a12cf54be89d85a63a89a
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2023-01-09 14:07:09 +0200

    Create ThreadFile.java

---
+++ ThreadFile.java
@@ -0,0 +1,39 @@
+package com.company;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ThreadFile extends Thread {
+    private String file;
+    private int lines = 0;
+
+    public ThreadFile (String file)
+    {
+        this.file = file;
+    }
+
+    public void run()
+    {
+        int count = 0;
+        try {
+            File file = new File(this.file);
+            Scanner Reader = new Scanner(file);
+            while (Reader.hasNextLine()) {
+                String data = Reader.nextLine();
+                //System.out.println(data);
+                count++;
+            }
+            Reader.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
+        this.lines = count;
+    }
+
+    public int getLines() throws InterruptedException {
+        return lines;
+    }
+}
