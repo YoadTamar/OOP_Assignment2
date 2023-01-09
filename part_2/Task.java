f6aed395f92694ad189a1442dac100398622f31c
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2023-01-09 20:37:29 +0200

    Create Task.java

---
+++ part_2/Task.java
@@ -0,0 +1,80 @@
+package com.company;
+import java.util.Objects;
+import java.util.concurrent.Callable;
+
+public class Task<T> implements Callable<T> , Comparable<Task<T>> {
+    TaskType taskType;
+    Callable <T> task;
+
+    public Task(Callable task , TaskType taskType) {
+        this.task = task;
+        this.taskType = taskType;
+    }
+
+    public Task(Callable task) {
+        this.task = task;
+        this.taskType.setPriority(3);
+    }
+
+    public TaskType getTaskType() {
+        return taskType;
+    }
+
+    public Callable<T> getTask() {
+        return task;
+    }
+
+    public void setTaskType(TaskType taskType) {
+        this.taskType = taskType;
+    }
+
+    public void setTask(Callable<T> task) {
+        this.task = task;
+    }
+
+
+    @Override
+    public T call() throws Exception {
+
+        try
+        {
+            return this.task.call();
+        }
+        catch (Exception e)
+        {
+                throw new Exception();
+        }
+    }
+
+    /**
+     *
+     * @param otherTask the object to be compared.
+     * @return 1 / -1 / 0 depends on if this taskType is larger smaller or even
+     *          to the object to be compared.
+     */
+    @Override
+    public int compareTo(Task<T> otherTask) {
+        return  Integer.compare(this.taskType.getPriorityValue() , otherTask.taskType.getPriorityValue());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(!(o instanceof Task)) return false;
+        Task<T> other = (Task)o;
+        if(this.task.equals(other.task) && this.taskType.getPriorityValue() == other.taskType.getPriorityValue())
+            return true;
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(task, taskType);
+    }
+
+    @Override
+    public String toString() {
+        return "{" + this.task.toString() +
+                "praiorty: " + taskType.getPriorityValue()
+                + "}";
+    }
+}
