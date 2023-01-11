1892ec0fb9ff01f96cee16412a4e6110a8c8b34d
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2023-01-11 13:54:25 +0200

    Update ThreadFile.java
    Add comments

--- src/Ex2_1/ThreadFile.java
+++ src/Ex2_1/ThreadFile.java
@@ -9,11 +9,18 @@ public class ThreadFile extends Thread {
     private String file;
     private int lines = 0;
 
+    /**
+     * constructor  , receive the file's name.
+     * @param file - String - file name
+     */
     public ThreadFile (String file)
     {
         this.file = file;
     }
 
+    /**
+     * This method calculate the numbers of the line in the file, and return it.
+     */
     public void run()
     {
         int count = 0;
@@ -34,13 +41,13 @@ public class ThreadFile extends Thread {
         this.lines = count;
     }
 
+    /**
+     * This method return the lines of the file
+     * @return  int - number of lines
+     * @throws InterruptedException
+     */
     public int getLines() throws InterruptedException {
         return lines;
     }
-
-
-
-
-
 }
 
