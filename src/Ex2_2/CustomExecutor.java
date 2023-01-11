4a8e198784b56652d5e6eb20f7d0e9fde477fb6b
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2023-01-11 15:13:59 +0200

    Update CustomExecutor.java
    add comments

--- src/Ex2_2/CustomExecutor.java
+++ src/Ex2_2/CustomExecutor.java
@@ -3,9 +3,12 @@ package Ex2_2;
 import java.util.concurrent.*;
 
 public class CustomExecutor extends ThreadPoolExecutor {
-    private int[] max = {-1, 0, 0, 0};
-    private final int maxp = 10;
+    private int[] max = {-1, 0, 0, 0}; /** The priority inside queue */
+    private final int maxp = 10; /** the default priority  */
 
+    /**
+     * constructor
+     */
     public CustomExecutor()
     {
         super(Runtime.getRuntime().availableProcessors()/2,
@@ -16,10 +19,16 @@ public class CustomExecutor extends ThreadPoolExecutor {
     }
 
 
+    /**
+     * Submits a value-returning Task for execution and returns a Future representing the pending results of the task.
+     * @param task – the Task to submit.
+     * @type <T> – the type of the task's result.
+     * @return a Future representing pending completion of the task.
+     * @throws IllegalArgumentException
+     */
     public <T> Future<T> submit(Task<T> task) throws IllegalArgumentException {
         if (task != null) {
             max[task.getTaskType().getPriorityValue()]++;
-
             AdapterCall adapterCall = new AdapterCall<>(task, task.getTaskType().getPriorityValue());
             super.execute(adapterCall);
             return adapterCall;
@@ -27,21 +36,43 @@ public class CustomExecutor extends ThreadPoolExecutor {
         throw new IllegalArgumentException();
     }
 
+    /**
+     * Submits a value-returning task for execution and returns a Future representing the pending results of the task.
+     * @param task - Callable<T> task
+     * @type <T> – the type of the task's result.
+     * @param taskType - enum TaskType
+     * @return a Future representing pending completion of the task.
+     */
     public <T> Future<T> submit(Callable<T> task , TaskType taskType) {
         return submit(Task.createTask(task , taskType));
     }
 
+    /**
+     * Submits a value-returning task for execution and returns a Future representing the pending results of the task.
+     * @param task - Callable<T> task
+     * @type <T> – the type of the task's result.
+     * @return a Future representing pending completion of the task.
+     */
     @Override
     public <T> Future<T> submit(Callable<T> task)
     {
         return submit(Task.createTask(task));
     }
 
+    /**
+     *This method activate before a task execute.
+     * @param t the thread that will run task {@code r}
+     * @param r the task that will be executed
+     */
     @Override
     protected void beforeExecute(Thread t, Runnable r) {
         if(r != null)  max[((AdapterCall) r).getP()]--;
     }
 
+    /**
+     * Return the max priority in queue
+     * @return nax priority in queue
+     */
     public int getCurrentMax()
     {
         boolean ok = false;
@@ -58,41 +89,76 @@ public class CustomExecutor extends ThreadPoolExecutor {
 
     }
 
+    /**
+     * Initiates an orderly shutdown in which previously submitted tasks are executed, but no new tasks will be accepted.
+     * Invocation has no additional effect if already shut down.
+     */
     public void gracefullyTerminate() {
         super.shutdown();
+        try
+        {
+            super.awaitTermination(50 , TimeUnit.MILLISECONDS);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
+    /**
+     * Sets the core number of threads.
+     * @param corePoolSize the new core size
+     */
     @Override
     public void setCorePoolSize(int corePoolSize) {
         super.setCorePoolSize(corePoolSize);
     }
 
+    /**
+     * Returns the core number of threads
+     * @return core number of threads
+     */
     @Override
     public int getCorePoolSize() {
         return super.getCorePoolSize();
     }
 
+    /**
+     * Sets the maximum allowed number of threads.
+     * @param maximumPoolSize the new maximum
+     */
     @Override
     public void setMaximumPoolSize(int maximumPoolSize) {
         super.setMaximumPoolSize(maximumPoolSize);
     }
 
+    /**
+     * Returns the maximum allowed number of threads.
+     * @return maximum allowed number of threads.
+     */
     @Override
     public int getMaximumPoolSize() {
         return super.getMaximumPoolSize();
     }
 
+    /**
+     * Returns a hash code value for the object.
+     * @return a hash code value for this object.
+     */
     @Override
     public int hashCode() {
         return super.hashCode();
     }
 
+    /**
+     * Returns a string identifying this pool.
+     * @return a string representation of the object.
+     */
     @Override
     public String toString() {
-        return "Queue: " + super.getQueue().toString() + "max priority in queue: " + getCurrentMax();
+        return  "Queue: " + super.getQueue().toString() +
+                "\nmax priority in queue: " + getCurrentMax();
     }
-
-
-
 }
+
+
 
